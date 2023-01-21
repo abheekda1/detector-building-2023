3a7388b82af3ba80dc80200904389cc718d7653f
Abheek Dhawan <abheekd@protonmail.com>
2023-01-20 20:25:29 -0600

    feat: add LED test

---
+++ ledtest.ino
@@ -0,0 +1,14 @@
+void setup() {
+  analogWrite(2, 255);
+  delay(1000);
+  analogWrite(2, 0);
+  delay(1000);
+  analogWrite(4, 255);
+  delay(1000);
+  analogWrite(4, 0);
+  delay(1000);
+  analogWrite(7, 255);
+  delay(1000);
+  analogWrite(7, 0);
+  delay(1000);
+}
