6404a2aca979c1e2333d849c50e1cce4d11673dc
Abheek Dhawan <abheekd@protonmail.com>
2023-03-30 22:31:41 -0500

    feat: add support for customizing calibration and LEDs

--- detector-building-2023.ino
+++ detector-building-2023.ino
@@ -1,9 +1,13 @@
 // default calibration mass count
-#define N 10
+#define N 20
+
+#define MAX_ZONES 10
 
 // ranges to be given at competition
-#define RANGE1_MAX 200
-#define RANGE2_MAX 400
+#define RANGE1_MAX 300
+#define RANGE2_MAX 600
+
+int numZones = 0;
 
 const int fsrPin = 0;     // the FSR and 10K pulldown are connected to a0
 int fsrReading;     // the analog reading from the FSR resistor divider
@@ -21,9 +25,20 @@ double y[N];
 // needed for ln func
 double a, b;
 
+// zones type
+struct zone {
+  bool red;
+  bool green;
+  bool blue;
+  int lowerBound;
+  int upperBound;
+};
+
+zone zones[MAX_ZONES];
+
 /* -----------------------*
- *  Helpers               *
- *------------------------*/
+    Helpers
+  ------------------------*/
 // maps analog data values to voltage
 int analogToVoltage(int data) {
   return map(data, 0, 1023, 0, 5000);
@@ -31,13 +46,13 @@ int analogToVoltage(int data) {
 
 // function to take ln easily
 double ln(double x) {
-    return log(x) / log(exp(1) /* e */);
+  return log(x) / log(exp(1) /* e */);
 }
 // end helpers
 
 /* -----------------------*
- *  Curve Fitting         *
- *------------------------*/
+    Curve Fitting
+  ------------------------*/
 // generates the curve using least squares
 // https://mathworld.wolfram.com/LeastSquaresFittingLogarithmic.html
 void genCurve(double x[], double y[], int n) {
@@ -62,8 +77,8 @@ double fitToCurve(int voltage) {
 
 
 /* -----------------------*
- *  LEDS                  *
- *------------------------*/
+    LEDS
+  ------------------------*/
 void turnOffLeds(void) {
   analogWrite(redPin, 0);
   analogWrite(greenPin, 0);
@@ -84,24 +99,77 @@ void turnOnBlue(void) {
 // end LEDs
 
 /* -----------------------*
- *  Main functions        *
- *------------------------*/
+    Main functions
+  ------------------------*/
 void setup(void) {
   // serial monitor
   Serial.begin(9600);
+
+  Serial.println("Please input all zones with color specifiers like 'RB' for Red-Blue then the bounds and finally send 'c' to continue:");
+
+  // loop through zones
+  for (int i = 0; true; i++) {
+    // read current line
+    while (!Serial.available()) {}
+    String s = Serial.readStringUntil('\n');
+
+    if (s == "c") {
+      // set numVals to values read so far instead
+      // of total values
+      numZones = i;
+      break;
+    }
+
+    bool red;
+    bool green;
+    bool blue;
+
+    for (int j = 0; j < s.length(); j++) {
+      if (s.charAt(i) == 'R') red = true;
+      else if (s.charAt(i) == 'G') green = true;
+      else if (s.charAt(i) == 'B') blue = true;
+    }
+
+    int lowerBound = 0;
+    int upperBound = 1000;
+
+    while (!Serial.available()) {}
+    Serial.print("Lower bound: ");
+    lowerBound = Serial.readStringUntil('\n').toInt();
+    Serial.println(lowerBound);
+    while (!Serial.available()) {}
+    Serial.print("Upper bound: ");
+    upperBound = Serial.readStringUntil('\n').toInt();
+    Serial.println(upperBound);
+
+    // create a new zone
+    zone newZone = {red, green, blue, lowerBound, upperBound};
+
+    // add to list of zones
+    zones[i] = newZone;
+
+    // print the values for clarity
+    Serial.println("New zone:\n\tLower bound: " + String(lowerBound) + "\n\tUpper bound: " + String(upperBound) + "\n\tColors: " + (red ? "R" : "") + (green ? "G" : "") + (red ? "B" : ""));
+  }
 
   // decrease numVals if loop is
   // exited early
   int numVals = N;
 
+  // set base values of platform
+  x[0] = 1;
+  fsrReading = analogRead(fsrPin);
+  int voltage = analogToVoltage(fsrReading);
+  y[0] = voltage;
+
+  Serial.println("Please calibrate the masses then send 'c' to continue:");
+
   // loop through calibration masses
-  for (int i = 0; i < N; i++) {
-    // wait for input
-    while(Serial.available() == 0) { }
-    
+  for (int i = 1; i <= N; i++) {
     // read current line
+    while (!Serial.available()) {}
     String s = Serial.readStringUntil('\n');
-    
+
     if (s == "c") {
       // set numVals to values read so far instead
       // of total values
@@ -125,6 +193,48 @@ void setup(void) {
 
     // print the values for clarity
     Serial.println("x[" + String(i) + "] = " + String(x[i]) + "\ny[" + String(i) + "] = " + String(y[i]) + "\n\n");
+  }
+
+  bool readyToGenCurve = false;
+
+  while (!readyToGenCurve) {
+    Serial.println("===================================");
+    for (int i = 0; i < numVals; i++) {
+      Serial.println(String(i + 1) + ")\n\tMass: " + String(x[i]) + "\n\tFSR Reading: " + String(y[i]));
+    }
+    Serial.println("===================================");
+    Serial.println();
+
+    String response = "";
+
+    while (response != "a" && response != "b") {
+      Serial.println("What would you like to do?\n\ta) Generate the curve!\n\tb) Replace a value");
+      while (!Serial.available()) {}
+      response = Serial.readStringUntil('\n');
+    }
+
+    if (response == "a") {
+      readyToGenCurve = true;
+    } else if (response == "b") {
+      Serial.print("Please input the index of the mass you would like to recalibrate: ");
+      while (!Serial.available()) {}
+      int idx = Serial.readStringUntil('\n').toInt() - 1;
+      Serial.println();
+
+      Serial.println("Please place the mass on the FSR and then input the weight in grams");
+
+      // get the mass in g as a double
+      // from input
+      while (!Serial.available()) {}
+      x[idx] = (double)Serial.readStringUntil('\n').toInt();
+
+      // get the corresponding voltage read
+      // from the FSR
+      fsrReading = analogRead(fsrPin);
+      int voltage = analogToVoltage(fsrReading);
+
+      y[idx] = voltage;
+    }
   }
 
   // generate a and b vals
@@ -157,14 +267,23 @@ void loop(void) {
 
   // turn on the LED corresponding to the current
   // mass range
-  if (mass < RANGE1_MAX) {
-    turnOnRed();
-  } else if (mass < RANGE2_MAX) {
-    turnOnGreen();
-  } else {
-    turnOnBlue();
+  //if (mass < RANGE1_MAX) {
+  //  turnOnBlue();
+  //} else if (mass < RANGE2_MAX) {
+  //  turnOnRed();
+  //} else {
+  //  turnOnGreen();
+  //}
+
+  for (int i = 0; i < numZones; i++) {
+    zone currentZone = zones[i];
+    if (mass >= currentZone.lowerBound && mass <= currentZone.upperBound) {
+      if (currentZone.red) turnOnRed();
+      if (currentZone.green) turnOnGreen();
+      if (currentZone.blue) turnOnBlue();
+    }
   }
-  
+
   delay(100);
 }
 // end main functions
